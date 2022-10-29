7d57953c48f4ee53107727e8a19c9a5bb866a2a5
Alex Mandebura <xenovidbis@gmail.com>
2022-10-29 19:48:24 +0300

    ligthbox render

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./js/gallery-items";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+function creatElemnt(gallery) {
+  const imageEl = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+    )
+    .join("");
+
+  return imageEl;
+}
+
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
